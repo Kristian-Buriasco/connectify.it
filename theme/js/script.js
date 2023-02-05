71de56396851a811613bc2684976754da9d78fe9
Lorenzo Marchisio <Lorenzomarchisio05@icloud.com>
2023-02-05 12:53:27 +0100

    removed unused code: venobox in js, clients logo slider in js
    removed unused dependecies:
    aos,
    venubox
    
    used cdn instead of local dependecies files

--- theme/js/script.js
+++ theme/js/script.js
@@ -5,155 +5,110 @@
  * GITHUB: https://github.com/themefisher/
  */
 
-(function ($) {
-  "use strict";
+// Sticky Menu
+$(window).scroll(function () {
+  const navigation = $(".navigation");
 
-  // Sticky Menu
-  $(window).scroll(function () {
-    if ($(".navigation").offset().top > 100) {
-      $(".navigation").addClass("nav-bg");
-    } else {
-      $(".navigation").removeClass("nav-bg");
-    }
+  const height = window.innerWidth > 768 ? 100 : 50;
+
+  navigation.toggleClass("nav-bg", navigation.offset().top > height);
+});
+
+// to center loader
+window.addEventListener("beforeunload", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "instant",
   });
+});
 
-  // loader fadeout
-  window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+// loader fadeout
+$(window).on("load", () => {
+  const page = (
+    window.location.pathname.split("/").at(-1) || "index.html"
+  ).replace(".html", "");
 
-  // loader fadeout
-  $(window).on("load", () => {
-    const page = (window.location.pathname.split("/").at(-1) || "index.html").replace(".html", "");
+  $(`.footer a[page-name="${page}"]`).attr("href", "#");
 
-    $(`.footer a[page-name="${page}"]`).attr("href", "#");
+  $(".loader-wrapper").fadeOut(400, () => $("body").css("overflow-y", "scroll"));
+});
 
-    $(".loader-wrapper").fadeOut(() => $("body").css("overflow-y", "scroll"));
-
-  });
-
-  // team slider
-  $(".team-slider").slick({
-    dots: false,
-    infinite: false,
-    speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow:
-      "<button type='button' class='prevArrow'><i class='ti-arrow-left'></i></button>",
-    nextArrow:
-      "<button type='button' class='nextArrow'><i class='ti-arrow-right'></i></button>",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
+// team slider
+$(".team-slider").slick({
+  dots: false,
+  infinite: false,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow:
+    "<button type='button' class='prevArrow'><i class='ti-arrow-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='nextArrow'><i class='ti-arrow-right'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
       },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-
-  // clients logo slider
-  $(".client-logo-slider").slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-
-  // about video popup
-  $(document).ready(function () {
-    $(".venobox").venobox();
-  });
-
-  // animation scroll js
-  var html_body = $("html, body");
-  $(".page-scroll").on("click", function () {
-    //use page-scroll class in any HTML tag for scrolling
-    if (
-      location.pathname.replace(/^\//, "") ===
-        this.pathname.replace(/^\//, "") &&
-      location.hostname === this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        html_body.animate(
-          {
-            scrollTop: target.offset().top - 50,
-          },
-          1500,
-          "easeInOutExpo"
-        );
-        return false;
-      }
-    }
-  });
-
-  // easeInOutExpo Declaration
-  jQuery.extend(jQuery.easing, {
-    easeInOutExpo: function (x, t, b, c, d) {
-      if (t === 0) {
-        return b;
-      }
-      if (t === d) {
-        return b + c;
-      }
-      if ((t /= d / 2) < 1) {
-        return (c / 2) * Math.pow(2, 10 * (t - 1));
-      }
-      +b;
-      return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
     },
-  });
-})(jQuery);
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+// animation scroll js
+let html_body = $("html, body");
+$(".page-scroll").on("click", function () {
+  //use page-scroll class in any HTML tag for scrolling
+  if (
+    location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
+    location.hostname === this.hostname
+  ) {
+    let target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      html_body.animate(
+        {
+          scrollTop: target.offset().top - 50,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+      return false;
+    }
+  }
+});
+
+// easeInOutExpo Declaration
+jQuery.extend(jQuery.easing, {
+  easeInOutExpo: function (x, t, b, c, d) {
+    if (t === 0) {
+      return b;
+    }
+    if (t === d) {
+      return b + c;
+    }
+    if ((t /= d / 2) < 1) {
+      return (c / 2) * Math.pow(2, 10 * (t - 1));
+    }
+    +b;
+    return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
+  },
+});
