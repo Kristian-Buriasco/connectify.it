70806bafa376dca688cd423b94b90a80dde5ef13
Lorenzo Marchisio <Lorenzomarchisio05@icloud.com>
2023-02-07 19:19:37 +0100

    implemented navbar collaps on link click, remove ugly scroll animation

--- theme/js/script.js
+++ theme/js/script.js
@@ -3,7 +3,7 @@
 $(window).scroll(function () {
   const navigation = $(".navigation");
 
-  const height = window.innerWidth > 768 ? 100 : 50;
+  const height = window.innerWidth > 768 ? 100 : 30;
 
   navigation.toggleClass("nav-bg", navigation.offset().top > height);
 });
@@ -67,44 +67,9 @@ $(".team-slider").slick({
   ],
 });
 
-// animation scroll js
-let html_body = $("html, body");
-$(".page-scroll").on("click", function () {
-  //use page-scroll class in any HTML tag for scrolling
-  if (
-    location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") &&
-    location.hostname === this.hostname
-  ) {
-    let target = $(this.hash);
-    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-    if (target.length) {
-      html_body.animate(
-        {
-          scrollTop: target.offset().top - 50,
-        },
-        1500,
-        "easeInOutExpo"
-      );
-      return false;
-    }
-  }
-});
-
-// easeInOutExpo Declaration
-jQuery.extend(jQuery.easing, {
-  easeInOutExpo: function (x, t, b, c, d) {
-    if (t === 0) {
-      return b;
-    }
-    if (t === d) {
-      return b + c;
-    }
-    if ((t /= d / 2) < 1) {
-      return (c / 2) * Math.pow(2, 10 * (t - 1));
-    }
-    +b;
-    return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
-  },
+// close the navbar on click of a link
+$('.navbar-nav li a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
 });
 
 
