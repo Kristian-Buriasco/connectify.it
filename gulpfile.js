acaafd6d5e17d926f80903e8bc9daee672deaa3b
Lorenzo Marchisio <Lorenzomarchisio05@icloud.com>
2023-03-25 16:47:14 +0100

    updated folder structure and dependencies based on new template version see https://github.com/connectify-it/template

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,16 @@
 
 const sass = require("gulp-sass")(require("sass"));
 const gulp = require("gulp");
-const gutil = require("gulp-util");
 const sourcemaps = require("gulp-sourcemaps");
 const fileinclude = require("gulp-file-include");
 const autoprefixer = require("gulp-autoprefixer");
 const bs = require("browser-sync").create();
 const rimraf = require("rimraf");
-const comments = require("gulp-header-comment");
 const jshint = require("gulp-jshint");
 const notify = require("gulp-notify");
 const plumber = require("gulp-plumber");
 
-var path = {
+const path = {
   src: {
     html: "source/*.html",
     others: "source/*.+(php|ico|png|xml|webmanifest|json|txt|htaccess|webp)",
@@ -83,7 +81,6 @@ gulp.task("js:build", function () {
       })
     )
     .pipe(jshint.reporter("jshint-stylish"))
-    .on("error", gutil.log)
     .pipe(gulp.dest(path.build.dirDev + "js/"))
     .pipe(
       bs.reload({
@@ -174,11 +171,12 @@ gulp.task(
 gulp.task(
   "build",
   gulp.series(
+    "clean",
     "html:build",
     "js:build",
     "scss:build",
     "images:build",
     "plugins:build",
-    "others:build"
+    "others:build",
   )
 );
